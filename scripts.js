5d4a28e9f0ed43776da1322d229995dfcee280ff
Chris Nguyen <chrishiepnguyen@yahoo.com>
2022-07-17 23:02:09 -0500

    added check blackjack

--- scripts.js
+++ scripts.js
@@ -1,43 +1,44 @@
-let dealerPoints = 0;
-let playerPoints = 0;
 let rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "ace", "jack", "queen", "king"];
 let suit = ["clubs", "diamonds", "hearts", "spades"];
+let dealerPoints = 0;
+let playerPoints = 0;
+
+let newGameBtn = document.getElementById('new-game');
+let hitBtn = document.getElementById('hit-button');
+let standBtn = document.getElementById('stand-button');
+let dealBtn = document.getElementById('deal-button');
 
 
 
-window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('new-game').disabled = true;
-    document.getElementById('hit-button').disabled = true;
-    document.getElementById('stand-button').disabled = true;
+window.addEventListener('DOMContentLoaded', () => {
+    newGameBtn.disabled = true;
+    hitBtn.disabled = true;
+    standBtn.disabled = true;
     buildDeck();
    shuffleDeck();
-})
+});
 
-function buildDeck() {
+let buildDeck = () => {
     deck = []
-
     for (let i = 0; i < rank.length; i++) {
         for (let j= 0; j < suit.length; j++) {
             deck.push(rank[i] + "_of_" + suit[j]);
-        }
-    }
-    console.log(deck);
-}
+        };
+    };
+};
 
-function shuffleDeck() {
+let shuffleDeck = () => {
     for (let i = 0; i < deck.length; i++) {
         let j = Math.floor(Math.random() * deck.length);
         let temp = deck[i];
         deck[i] = deck[j];
         deck[j] = temp;
-    }
-    console.log(deck);
-}
+    };
+};
 
 
-document.getElementById('deal-button').addEventListener('click', dealGame);
-
-function dealGame () {
+//DEAL BUTTON
+dealBtn.addEventListener('click', () => {
     for (let i = 0; i < 1; i++){
         let cardImg = document.createElement("img");
         let card = deck.shift();
@@ -49,6 +50,7 @@ function dealGame () {
         let cardImg = document.createElement("img");
         let hiddenCard = deck.shift();
         cardImg.src = "./images/backCard.png";
+        // cardImg.src = "./images/" + hiddenCard + ".png";
         dealerPoints += calculatePoints(hiddenCard);
         document.getElementById("dealer-hand").append(cardImg);
     }
@@ -65,17 +67,36 @@ function dealGame () {
         cardImg.src = "./images/" + card + ".png";
         dealerPoints += calculatePoints(card);
         document.getElementById("dealer-hand").append(cardImg);
-    }
-    document.getElementById('deal-button').disabled = true;
-    document.getElementById('new-game').disabled = true;
-    document.getElementById('hit-button').disabled = false;
-    document.getElementById('stand-button').disabled = false;
-    document.getElementById('player-points').innerText = playerPoints;
-    document.getElementById('dealer-points').innerText = "??";
-   
-}
+    };
 
-function calculatePoints(card) {
+    if (dealerPoints === 21) {
+        setTimeout(function(){alert('Dealer blackjack! Dealer wins!');},500);
+        dealBtn.disabled = true;
+        newGameBtn.disabled = false;
+        hitBtn.disabled = true;
+        standBtn.disabled = true;
+        document.getElementById('player-points').innerText = playerPoints;
+        document.getElementById('dealer-points').innerText = dealerPoints;
+    } else if (playerPoints === 21) {
+        setTimeout(function(){alert('WINNER WINNER CHICKEN DINNER! Player wins!');},500);
+        dealBtn.disabled = true;
+        newGameBtn.disabled = false;
+        hitBtn.disabled = true;
+        standBtn.disabled = true;
+        document.getElementById('player-points').innerText = playerPoints;
+        document.getElementById('dealer-points').innerText = dealerPoints;
+    } else {
+        dealBtn.disabled = true;
+        newGameBtn.disabled = true;
+        hitBtn.disabled = false;
+        standBtn.disabled = false;
+        document.getElementById('player-points').innerText = playerPoints;
+        document.getElementById('dealer-points').innerText = "??";
+    };
+});
+
+
+let  calculatePoints = (card) => {
     let data = card.split("_of_");
     let value = data[0];
     if (isNaN(value)) {
@@ -87,9 +108,8 @@ function calculatePoints(card) {
     return parseInt(value);
 }
 
-
-document.getElementById('hit-button').addEventListener('click', hitCard);
-function hitCard () {
+//HIT BUTTON
+hitBtn.addEventListener('click', () => {
     let cardImg = document.createElement("img");
     let card = deck.shift();
     cardImg.src = "./images/" + card + ".png";
@@ -99,14 +119,14 @@ function hitCard () {
         if (playerPoints > 21) {
             setTimeout(function(){alert('You busted! Dealer wins!');},500);
             document.getElementById('dealer-points').innerText = dealerPoints;
-            document.getElementById('hit-button').disabled = true;
-            document.getElementById('stand-button').disabled = true;
-            document.getElementById('new-game').disabled = false;
+            hitBtn.disabled = true;
+            standBtn.disabled = true;
+            newGameBtn.disabled = false;
     }
-}
+});
 
-document.getElementById('stand-button').addEventListener('click', stand);
-function stand () {
+//STAND BUTTON
+standBtn.addEventListener('click', () => {
     document.getElementById('dealer-points').innerText = dealerPoints;
     if (dealerPoints < 17) {
         let cardImg = document.createElement("img");
@@ -149,12 +169,11 @@ function stand () {
     } else { (playerPoints === dealerPoints)
         setTimeout(function(){alert('Tie Game!');},500);
     }
-    document.getElementById('hit-button').disabled = true;
-    document.getElementById('stand-button').disabled = true;
-    document.getElementById('new-game').disabled = false;
-}
+    hitBtn.disabled = true;
+    standBtn.disabled = true;
+    newGameBtn.disabled = false;
+});
 
-document.getElementById('new-game').addEventListener('click', restartGame);
-function restartGame() {
+newGameBtn.addEventListener('click', () => {
     location.reload();
-}
+});
